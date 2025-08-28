bc139c0d31c8faff1bde3bc5b00db769592d288a
Rohith-121 <rohith@erebortech.com>
2025-08-28 11:08:55 +0530

    Delete ResponseModel.js

--- ResponseModel.js
+++
@@ -1,9 +0,0 @@
-class ResponseModel {
-  constructor(success = false, message = "", data = null) {
-    this.success = success;
-    this.message = message;
-    this.data = data;
-  }
-}
-
-module.exports = ResponseModel;
